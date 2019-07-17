20695de4c01b0143476c9c6c5a929aa3c7134df6
srikamal <srikamal2001@gmail.com>
2019-07-17 13:32:51 +0800

    ver 1.2 - made class Lakshmi more generic           wrote down more documentation

--- src/Lakshmi.java
+++ src/Lakshmi.java
@@ -5,22 +5,30 @@ import java.util.Map;
  */
 public class Lakshmi
 {
-    // A representation of the NYSE as a Map stock code-stock
-    private Map<String, Stock> NYSE;
+    // A representation of the NYSE as a Map <stock code listing of stock>
+    private Map<String, Stock> Stock_Market;
 
     // A standard constructor
     public Lakshmi(Map<String, Stock> NYSE)
     {
-        this.NYSE = NYSE;
+        this.Stock_Market = NYSE;
     }
 
     /**
-     * Returns the NYSE stock market
-     * @return NYSE
+     * Returns the stock market
+     * @return stock market
      */
     public Map<String, Stock> getNYSE()
     {
-        return NYSE;
+        return this.Stock_Market;
     }
+
+    /**
+     * More code to come here that deals with
+     *  1. dividing companies into small/mid/large cap values
+     *  2. top 100 companies
+     *  3. bottom 100 companies
+     *  4. return a individual stock
+     */
 
 }
