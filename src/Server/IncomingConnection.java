398dbdc1f124373293387053fc729ce35cdde602
chrcheney <chr.cheney@gmail.com>
2019-07-28 21:59:14 -0400

    Outline of command handling
    Sri you need to name your classes better

--- src/Server/IncomingConnection.java
+++ src/Server/IncomingConnection.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.net.Socket;
 import java.util.NoSuchElementException;
 import common.Duplexer;
+import common.Kumbhakarna;
 
 class IncomingConnection extends Thread{
     private Duplexer connection;
@@ -14,15 +15,24 @@ class IncomingConnection extends Thread{
     }
 
     public void run(){
+        connection.send(Kumbhakarna.CONNECT);
         //Primary loop, process incoming requests here
         try{
             while (true){
                 String message = connection.receive();
                 System.out.println("Message: " + message);
-                //TODO: Add command handling, once commands are decided
-                //Temporary, for testing purposes
-                connection.send(message);
+                String[] tokens = message.split(" ");
+                switch (tokens[0]){
+                    case Kumbhakarna.ADD:
+                        break;
+                    case Kumbhakarna.DROP:
+                        break;
+                    case Kumbhakarna.STOP:
+                        break;
+                }
             }
-        } catch (NoSuchElementException ex){}
+        } catch (Exception ex){
+            connection.send(Kumbhakarna.ERROR);
+        }
     }
 }
