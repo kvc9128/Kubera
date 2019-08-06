4c60c02b10e742ad48f3b005878d1268ca0c9622
srikamal <srikamal2001@gmail.com>
2019-08-06 12:20:10 +0800

    comments updated to reflect new status

--- src/Server/Raavan.java
+++ src/Server/Raavan.java
@@ -19,38 +19,6 @@ import java.util.logging.Level;
  * to get daily stock information. This class will be responsible for gathering and storing the
  * stock tickers for all the stocks in the NYSE. This class will then call other classes
  * to analyse the data.
- *
- * Step1: Class Server.Raavan
- *          1. Get stocks from EODDATA - Complete
- *          2. Store them in appropriate data structure - complete
- *              a. Small mid large cap division - TBD
- *              b. top 100 companies - TBD
- *              c. bottom 100 companies -TBD
- *              d. Average Market Capitalization -TBD
- *
- * Step2: Class X - TBD
- *          1. Represent a user's portfolio and hold a bunch of stocks that the user wants
- *          2. Allow the user to track and follow his stocks and portfolio
- *          3. Represent user's wealth and resources
- *          4. Offer a visual representation of portfolio.(possible GUI)
- *          5. Class Z can be used here to further augment stock's value
- *
- * Step3: Class Y
- *          1. Machine learning model will be here
- *          2. Training set and testing set will be stored here
- *          3. Buy EOD NYSE 5Yr records and build a system to predict stock price
- *          4. The goal will be to predict a company;s stock price based on various factors
- *          5. Aim to be 0.80 or 1.20 of NYSE Composite
- *          6. We will use the training set to teach the model
- *          7. The testing model will be used to check the model
- *
- * Step4: Class Z
- *          1. This is a supporting class for class Y
- *          2. This class will be similar to raavan in the sense that it will also be accessing
- *             the web and be a social media taste checker
- *          3. This class will scan twitter to check for financial updates, company's
- *             trending values etc.
- *          4. Possible use of neural nets here
  */
 public class Raavan
 {
